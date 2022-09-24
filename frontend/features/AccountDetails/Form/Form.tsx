25ff9ca549ba450dd66752a3bd03e1c49674884d
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2022-09-25 00:49:03 +0530

    refactor: moved Form in AccDetails as their own feature

---
+++ frontend/features/AccountDetails/Form/Form.tsx
@@ -0,0 +1,103 @@
+// COMMT: Moved from frontend\pages\account\details\index.tsx
+
+import { FC, useEffect, useState, ChangeEvent } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import styles from './Form.module.css';
+
+interface IFormProps {
+  formAccDetails: { [key:string]:string|number }
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: () => void;
+  handleReset: () => void;
+}
+
+const Form: FC<IFormProps> = (props) => {
+
+  return (
+    <div className={styles['form']}>
+      <div className={styles['inputs-container']}>
+        <TextField
+          id='firstName'
+          label='First Name'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          value={props.formAccDetails?.firstName}
+          onChange={props.handleChange}
+        />
+        <TextField
+          id='lastName'
+          label='Last Name'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          value={props.formAccDetails?.lastName}
+          onChange={props.handleChange}
+        />
+        <TextField
+          id='displayName'
+          label='Display Name'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          required={true}
+          value={props.formAccDetails?.displayName}
+          onChange={props.handleChange}
+        />
+        <TextField
+          id='email'
+          label='Email'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          required={true}
+          value={props.formAccDetails?.email}
+          onChange={props.handleChange}
+        />
+        <TextField
+          id='phoneNo1'
+          type='number'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          label='Phone No (Work)'
+          value={props.formAccDetails?.phoneNo1}
+          onChange={props.handleChange}
+        />
+        <TextField
+          id='phoneNo2'
+          type='number'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          label='Phone No (Work)'
+          value={props.formAccDetails?.phoneNo2}
+          onChange={props.handleChange}
+        />
+        <TextField
+          id='location'
+          label='Location'
+          InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
+          color='secondary'
+          value={props.formAccDetails?.location}
+          onChange={props.handleChange}
+        />
+
+
+      </div>
+      <div className={styles['buttons-container']}>
+        <Button
+          variant='contained'
+          onClick={props.handleSubmit}
+          >
+          Save Changes
+        </Button>
+
+        <Button
+          variant='outlined'
+          onClick={props.handleReset}
+          >
+          Reset
+        </Button>
+
+      </div>
+    </div>
+  )
+};
+
+export default Form;
--- frontend/pages/account/details/index.tsx
+++ frontend/pages/account/details/index.tsx
@@ -1,6 +1,5 @@
 import { FC, useEffect, useState, ChangeEvent } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Form from '../../../features/AccountDetails/Form/Form';
 import styles from './index.module.css';
 
 interface IAccountDetailsProps {
@@ -42,88 +41,12 @@ const AccountDetails: FC<IAccountDetailsProps> = (props) => {
         My Profile
       </div>
       <div className={styles['body']}>
-        <div className={styles['form']}>
-          <div className={styles['inputs-container']}>
-            <TextField
-              id='firstName'
-              label='First Name'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              value={formAccDetails.firstName}
-              onChange={handleChange}
-            />
-            <TextField
-              id='lastName'
-              label='Last Name'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              value={formAccDetails.lastName}
-              onChange={handleChange}
-            />
-            <TextField
-              id='displayName'
-              label='Display Name'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              required={true}
-              value={formAccDetails.displayName}
-              onChange={handleChange}
-            />
-            <TextField
-              id='email'
-              label='Email'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              required={true}
-              value={formAccDetails.email}
-              onChange={handleChange}
-            />
-            <TextField
-              id='phoneNo1'
-              type='number'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              label='Phone No (Work)'
-              value={formAccDetails.phoneNo1}
-              onChange={handleChange}
-            />
-            <TextField
-              id='phoneNo2'
-              type='number'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              label='Phone No (Work)'
-              value={formAccDetails.phoneNo2}
-              onChange={handleChange}
-            />
-            <TextField
-              id='location'
-              label='Location'
-              InputLabelProps={{ style: {color: 'hsl(0, 0%, 22%)'} }}
-              color='secondary'
-              value={formAccDetails.location}
-              onChange={handleChange}
-            />
-
-
-          </div>
-          <div className={styles['buttons-container']}>
-            <Button
-              variant='contained'
-              onClick={handleSubmit}
-              >
-              Save Changes
-            </Button>
-
-            <Button
-              variant='outlined'
-              onClick={handleReset}
-              >
-              Reset
-            </Button>
-
-          </div>
-        </div>
+        <Form
+          formAccDetails={formAccDetails}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          handleReset={handleReset}
+        />
         <div className={styles['result']}>
           <div className={styles['image-holder']}>
 
