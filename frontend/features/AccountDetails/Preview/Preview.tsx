56b7c849942f8d6defca4c7287b0e5fd5c145990
Shahul Hammeed <53307156+shahul01@users.noreply.github.com>
2022-09-26 23:03:29 +0530

    feat:fe: added fake image placeholder on form preview

--- frontend/features/AccountDetails/Preview/Preview.tsx
+++ frontend/features/AccountDetails/Preview/Preview.tsx
@@ -1,5 +1,6 @@
 // COMMT: Moved from: frontend\pages\account\details\index.tsx
 
+import Image from 'next/image';
 import { FC, useEffect, useState } from 'react';
 import { IFormAccDetails } from '../../../types/global';
 import styles from './Preview.module.css';
@@ -9,10 +10,29 @@ interface IPreviewProps {
 }
 
 const Preview: FC<IPreviewProps> = (props) => {
+  const imagesArr = props.formAccDetails?.images;
+  const profileImage = imagesArr?.find((currImg:{}) => !currImg.isThumbnail)||'';
 
   return (
     <div className={styles['preview']}>
-        <div className={styles['image-holder']}>
+        <div className={styles['image-button-container']}>
+          <div className={styles['image-container']}>
+            {profileImage ? (
+              <Image
+                src={profileImage?.data}
+                alt='Profile Image'
+                layout='fill'
+                objectFit='cover'
+                // height='200'
+                // width='200'
+              />) : (
+                <div className={styles['empty-image']}></div>
+              )
+            }
+          </div>
+          <div className={styles['button']}>
+
+          </div>
 
         </div>
         <div className={styles['text-details']}>
